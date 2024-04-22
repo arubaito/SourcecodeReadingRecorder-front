5d5fbb366c00d8fad59382fb5c9fd719ee0caa3a
arubaito <asdfghjk0330@gmail.com>
2024-04-22 15:34:55 +0900

    (S01)エラーが出てややこしいのでとりあえずS01_home.jsに書かれていたをS01_Record.jsに移行
    ↓これがでる
     ⨯ Error: Event handlers cannot be passed to Client Component props.
      <span class=... onClick={function onClickDateRegister} children=...>
                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    If you need interactivity, consider converting part of this to a
    Client Component.

--- src/components/S01_Record.js
+++ src/components/S01_Record.js
@@ -1,10 +1,71 @@
-'use client'; // クライアント側でイベントハンドラを登録する
-
-import { onClickDateRegister, onClickStatus } from "@/script/S01_home";
+'use client'
 
 export default function Record({category, sourcefile, status, completeDate}) {
-    return (
+    
+    // 完了日の登録ボタンを押した時のハンドラ
+    const onClickDateRegister = (e) => {
+        console.log(e)
+    
+        // 当日の日付を取得
+        let today = new Date();
+        let year = today.getFullYear();
+        let month = ("0" + (today.getMonth() + 1)).slice(-2);
+        let date = ("0" + (today.getDate())).slice(-2);
+    
+        let initDate = year + "-" + month + "-" + date;
+    
+        e.target.outerHTML = `<span><input type=\"date\" /></span>`;
+    }
 
+    // 状態のボタンを押した時のハンドラ
+    const onClickStatus = (e) => {
+
+
+        
+        let status = e.target.innerHTML;
+    
+        // 未対応-> 処理中
+        if (status == "未対応") {
+    
+            e.target.outerHTML = "<span class=\"status status-active\">処理中</span>"
+    
+            // 変更した要素にハンドラ登録
+            let statuses = document.getElementsByClassName('status')
+            for (let i = 0; i < statuses.length; i++) {
+                statuses[i].addEventListener("click", onClickStatus)
+            }
+    
+            //TODO DB登録処理
+    
+            // 処理中-> 処理済み
+        } else if (status == "処理中") {
+    
+            e.target.outerHTML = "<span class=\"status status-complete\">処理済み</span>"
+    
+            // 変更した要素にハンドラ登録
+            let statuses = document.getElementsByClassName('status')
+            for (let i = 0; i < statuses.length; i++) {
+                statuses[i].addEventListener("click", onClickStatus)
+            }
+    
+            //TODO DB登録処理
+    
+        } else if (status == "処理済み") {
+    
+            e.target.outerHTML = "<span class=\"status status-inactive\">未対応</span>"
+    
+            // 変更した要素にハンドラ登録
+            let statuses = document.getElementsByClassName('status')
+            for (let i = 0; i < statuses.length; i++) {
+                statuses[i].addEventListener("click", onClickStatus)
+            }
+    
+            //TODO DB登録処理
+    
+        }
+    }
+
+    return (
         <>
             <tr>
                 <td>
@@ -12,24 +73,18 @@ export default function Record({category, sourcefile, status, completeDate}) {
                 </td>
                 <td class="source">{sourcefile}</td>
                 <td>
-                    <CreateStatus status={status} />
+                    <CreateStatus status={status} onClickStatus={onClickStatus} />
                 </td>
-                <td>{
-                    // propsで日付が設定されていたら日付を、設定されていなければ登録ボタンを表示する
-                    completeDate ? 
-                        completeDate : 
-                        (<span class="date-register" onClick={onClickDateRegister}>登録</span>)
-                    }
+                <td>
+                    <CreateCompleteDate completeDate={completeDate}  onClickDateRegister={onClickDateRegister}/> 
                 </td>
             </tr>
-
-
         </>
     )
 }
 
 // 種別の要素を生成するコンポーネント
-function CreateCategory({category}){
+async function CreateCategory({category}){
 
     // Java
     if(category == "1"){
@@ -39,12 +94,23 @@ function CreateCategory({category}){
 
 }
 
+// 完了日の要素を生成するコンポーネント
+function CreateCompleteDate({completeDate, onClickDateRegister}){
+
+    if(completeDate != undefined){
+        return completeDate;
+    } else {
+        return (<span class="date-register" onClick={onClickDateRegister}>登録</span>);
+    }
+}
+
+
 // 状態の要素を生成するコンポーネント
-function CreateStatus({status}){
+function CreateStatus({status, onClickStatus}){
 
     if(status == "1"){
 
-        return (<span class="status status-inactive"onClick={onClickStatus}>未対応</span>)
+        return (<span class="status status-inactive" onClick={onClickStatus}>未対応</span>)
 
     } else if (status == "2"){
         
@@ -55,4 +121,15 @@ function CreateStatus({status}){
         return (<span class="status status-complete" onClick={onClickStatus}>処理済み</span>)
 
     }        
+}
+
+// 全てのソースファイルの情報をサーバから取得する関数
+export async function getAllSourcefile(){
+    try {
+        const response = await fetch("http://localhost:8080/get-all-sourcefile");
+        const res = await response.json();
+        return res;
+    }catch(err){
+        console.log(err);
+    } 
 }
