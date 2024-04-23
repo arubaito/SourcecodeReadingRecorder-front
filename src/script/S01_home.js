2f1d2423cb2dd00d16ab95e9c59d2bb4f90189e3
arubaito <asdfghjk0330@gmail.com>
2024-04-23 15:25:55 +0900

    (S01)リファクタリング

--- src/script/S01_home.js
+++ src/script/S01_home.js
@@ -1,10 +1,12 @@
 // 全てのソースファイルの情報をサーバから取得する関数
-export async function getAllSourcefile(){
+export async function getAllSourcefile() {
     try {
+
         const response = await fetch("http://localhost:8080/get-all-sourcefile");
         const res = await response.json();
         return res;
-    }catch(err){
+    } catch (err) {
+
         console.log(err);
-    } 
+    }
 }
