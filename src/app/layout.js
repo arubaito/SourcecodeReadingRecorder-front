e5bb7a9727655d0c0b375854cc77edc45e284d00
arubaito <asdfghjk0330@gmail.com>
2024-04-23 14:12:27 +0900

    (全般)フォントをADLaM_Displayに変更

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { ADLaM_Display, Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = ADLaM_Display({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
   title: "Sourcecode Reading Recorder",
