e004c8f1e98427e6e8a2ac12352808a066b2f3ea
arubaito <asdfghjk0330@gmail.com>
2024-05-13 14:16:33 +0900

    (S01)トップページをなくして/homeにリダイレクトするように修正（気分）

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,5 @@
-import Table from "@/components/S01_Table";
-import { getAllPackage, getAllSourcefile } from "@/script/S01_home";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-
-  // 初期表示処理でソースファイルの一覧を取得する
-  let allSourcefile = await getAllSourcefile();
-  console.log("------allSourcefile------")
-  console.log(allSourcefile)
-  console.log("------------")
-
-  // 初期表示処理でパッケージの一覧を取得する
-  let allPackage = await getAllPackage();
-  console.log("------allPackage------")
-  console.log(allPackage)
-  console.log("------------")
-
-  return (
-    <main>
-      <header>
-        <h1 className="title">SourceCode Reading Recorder</h1>
-      </header>
-
-      {/* <!-- セレクトボックスとテーブル --> */}
-      <div className="result-set">
-
-        <Table allSourcefile={allSourcefile} allPackage={allPackage} />
-      
-      </div>
-    </main>
-  );
+  redirect("./home")
 }
